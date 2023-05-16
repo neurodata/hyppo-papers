f718c4b3c0b7cd428ff217238bfa3fa2544165f5
Sambit Panda <sampanda501@gmail.com>
2023-05-16 19:56:05 -0400

    use fast kmerf power instead

--- kmerf/power.py
+++ kmerf/power.py
@@ -21,23 +21,39 @@ def _perm_stat(test, sim, n=100, p=1, noise=True):
     Generates null and alternate distributions
     """
     x, y = _indep_sim_gen(sim, n, p, noise=noise)
-    obs_stat = test.statistic(x, y)
+    obs_stat = test().statistic(x, y)
     permy = np.random.permutation(y)
-    perm_stat = test.statistic(x, permy)
+    perm_stat = test().statistic(x, permy)
 
     return obs_stat, perm_stat
+
+
+def _nonperm_pval(test, sim, n=100, p=1, noise=True):
+    """
+    Generates fast  permutation pvalues
+    """
+    x, y = _indep_sim_gen(sim, n, p, noise=noise)
+    pvalue = test().test(x, y)[1]
+
+    return pvalue
 
 
 def power_indep(test, sim, n=100, p=1, noise=True, alpha=0.05, reps=1000):
     """
     Calculates empirical power
     """
-    test = test()
-    alt_dist, null_dist = map(
-        np.float64,
-        zip(*[_perm_stat(test, sim, n, p, noise=noise) for _ in range(reps)]),
-    )
-    cutoff = np.sort(null_dist)[ceil(reps * (1 - alpha))]
-    empirical_power = (1 + (alt_dist >= cutoff).sum()) / (1 + reps)
+    test_name = test.__name__
+    if test_name == "KMERF":
+        pvals = np.array([
+            _nonperm_pval(test, sim, n, p, noise=noise) for _ in range(reps)
+        ])
+        empirical_power = (1 + (pvals <= alpha).sum()) / (1 + reps)
+    else:
+        alt_dist, null_dist = map(
+            np.float64,
+            zip(*[_perm_stat(test, sim, n, p, noise=noise) for _ in range(reps)]),
+        )
+        cutoff = np.sort(null_dist)[ceil(reps * (1 - alpha))]
+        empirical_power = (1 + (alt_dist >= cutoff).sum()) / (1 + reps)
 
     return empirical_power
--- kmerf/power_2samp.py
+++ kmerf/power_2samp.py
@@ -26,11 +26,25 @@ def _perm_stat(test, test_name, sim, n=100, p=1, noise=True):
         x, y = k_sample_transform([u, v], test_type="rf")
     else:
         x, y = k_sample_transform([u, v])
-    obs_stat = test.statistic(x, y)
+    obs_stat = test().statistic(x, y)
     permy = np.random.permutation(y)
-    perm_stat = test.statistic(x, permy)
+    perm_stat = test().statistic(x, permy)
 
     return obs_stat, perm_stat
+
+
+def _nonperm_pval(test, test_name, sim, n=100, p=1, noise=True):
+    """
+    Generates fast  permutation pvalues
+    """
+    u, v = rot_ksamp(sim, n, p, noise=noise, pow_type="dim")
+    if test_name == "KMERF":
+        x, y = k_sample_transform([u, v], test_type="rf")
+    else:
+        x, y = k_sample_transform([u, v])
+    pvalue = test().test(x, y)[1]
+
+    return pvalue
 
 
 def power_ksamp(test, sim, n=100, p=1, noise=True, alpha=0.05, reps=1000):
@@ -38,12 +52,17 @@ def power_ksamp(test, sim, n=100, p=1, noise=True, alpha=0.05, reps=1000):
     Calculates empirical power
     """
     test_name = test.__name__
-    test = test()
-    alt_dist, null_dist = map(
-        np.float64,
-        zip(*[_perm_stat(test, test_name, sim, n, p, noise=noise) for _ in range(reps)]),
-    )
-    cutoff = np.sort(null_dist)[ceil(reps * (1 - alpha))]
-    empirical_power = (1 + (alt_dist >= cutoff).sum()) / (1 + reps)
+    if test_name == "KMERF":
+        pvals = np.array([
+            _nonperm_pval(test, test_name, sim, n, p, noise=noise) for _ in range(reps)
+        ])
+        empirical_power = (1 + (pvals <= alpha).sum()) / (1 + reps)
+    else:
+        alt_dist, null_dist = map(
+            np.float64,
+            zip(*[_perm_stat(test, test_name, sim, n, p, noise=noise) for _ in range(reps)]),
+        )
+        cutoff = np.sort(null_dist)[ceil(reps * (1 - alpha))]
+        empirical_power = (1 + (alt_dist >= cutoff).sum()) / (1 + reps)
 
     return empirical_power
