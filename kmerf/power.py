ceb6c02530b46e8f32a78c0ece64b9da87a4b861
Sambit Panda <sampanda501@gmail.com>
2023-10-20 23:24:27 -0400

    change sine 16 to sine 8 vs independence kmerf

--- kmerf/power.py
+++ kmerf/power.py
@@ -3,6 +3,68 @@ from math import ceil
 
 from hyppo.tools import indep_sim
 
+def _random_uniform(n, p, low=-1, high=1):
+    """Generate random uniform data."""
+    return np.array(np.random.uniform(low, high, size=(n, p)))
+
+def _calc_eps(n):
+    """Calculate noise."""
+    return np.random.normal(0, 1, size=(n, 1))
+
+def _sin(n, p, noise=False, low=-1, high=1, period=4 * np.pi):
+    """Helper function to calculate sine simulation"""
+    x = _random_uniform(n, p, low, high)
+    if noise:
+        sig = np.identity(p)
+        v = np.random.multivariate_normal(np.zeros(p), sig, size=n)
+        x = x + 0.02 * p * v
+    eps = _calc_eps(n)
+
+    if period == 4 * np.pi:
+        cc = 1
+    else:
+        cc = 0#.0005
+
+    y = np.sin(x * period) + cc * noise * eps
+
+    return x, y
+
+
+def sin_eight_pi(n, p, noise=False, low=-1, high=1):
+    r"""
+    Sine 8\ :math:`\pi` simulation.
+
+    Sine 8\ :math:`\pi` :math:`(X, Y) \in \mathbb{R}^p \times \mathbb{R}^p`:
+    :math:`U \sim \mathcal{U}(-1, 1)`, :math:`V \sim \mathcal{N}(0, 1)^p`,
+    :math:`\theta = 16 \pi`,
+
+    .. math::
+
+        X_{|d|} &= U + 0.02 p V_{|d|}\ \mathrm{for}\ d = 1, ..., p \\
+        Y &= \sin (\theta X) + \kappa \epsilon
+
+    Parameters
+    ----------
+    n : int
+        The number of samples desired by the simulation (>= 5).
+    p : int
+        The number of dimensions desired by the simulation (>= 1).
+    noise : bool, default: False
+        Whether or not to include noise in the simulation.
+    low : float, default: 1
+        The lower limit of the uniform distribution simulated from.
+    high : float, default: 1
+        The upper limit of the uniform distribution simulated from.
+
+    Returns
+    -------
+    x,y : ndarray of float
+        Simulated data matrices. ``x` and ``y`` have shapes ``(n, p)`` and ``(n, p)``
+        where `n` is the number of samples and `p` is the number of
+        dimensions.
+    """
+    return _sin(n, p, noise=noise, low=low, high=high, period=8 * np.pi)
+
 
 def _indep_sim_gen(sim, n, p, noise=True):
     """
@@ -10,6 +72,8 @@ def _indep_sim_gen(sim, n, p, noise=True):
     """
     if sim in ["multiplicative_noise", "multimodal_independence"]:
         x, y = indep_sim(sim, n, p)
+    elif sim == "sin_eight_pi":
+        x, y = sin_eight_pi(n, p, noise=noise)
     else:
         x, y = indep_sim(sim, n, p, noise=noise)
 
@@ -54,7 +118,7 @@ def power_indep(test, sim, n=100, p=1, noise=True, alpha=0.05, reps=1000, **test
     Calculates empirical power
     """
     test_name = test.__name__
-    if test_name == "KMERF":
+    if test_name in ["KMERF", "Dcorr", "Hsic"]:
         if sim == "causal":
             pvals = list(map(
                 np.float64,
